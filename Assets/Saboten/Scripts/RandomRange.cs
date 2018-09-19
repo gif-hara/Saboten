a063c70e5dcd3ee3cfd099bd58b792faae0a18cc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-09-19 22:59:16 +0900

    :new: RandomRange実装

---
+++ Assets/Saboten/Scripts/RandomRange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Saboten
+{
+    /// <summary>
+    /// 指定した値からランダムな値を制御するクラス
+    /// </summary>
+    [Serializable]
+    public sealed class RandomRange
+    {
+        [SerializeField]
+        private float min;
+
+        [SerializeField]
+        private float max;
+
+        public float Evalute
+        {
+            get
+            {
+                return UnityEngine.Random.Range(this.min, this.max);
+            }
+        }
+    }
+}
